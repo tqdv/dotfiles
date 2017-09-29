ce175da3d557df5ade327f9486bfac131716f60c
Tilwa Qendov <tilwa.qendov@gmail.com>
2017-09-29 22:24:27 +0000

    Add a few shell aliases

---
+++ bash/alias.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Sorted Disk Usage
+sdu () {
+	if [ $# -eq 0 ]
+	then
+		du -xhd 1 . | sort -h;
+	else
+		du -xhd 1 "$@" | sort -h;
+	fi
+}
+
+alias rdf="df -h /" # Root filesystem disk usage
---
+++ bash/opensuse.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+alias ddup="zypper dist-upgrade --details" # Detailed dist-upgrade
