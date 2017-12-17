37dda0d5e8a63b781ed60d4a3859e3916e75db24
Tilwa Qendov <tilwa.qendov@gmail.com>
2017-12-17 23:04:25 +0100

    Fix typo and rearrange

--- Scripts/alias.sh
+++ Scripts/alias.sh
@@ -8,14 +8,12 @@
 # -d1 --max-depth=1
 sdu () {
 	if [ $# -eq 0 ]
-	then
-		du -aDhxd1 . | sort -h;
-	else
-		du -aDhxd1 "$@" | sort -h;
+	then du -aDhxd1 . | sort -h;
+	else du -aDhxd1 "$@" | sort -h;
 	fi
 }
 
-# Appends a slash if the 2nd tab-seperated field is a directory
+# Appends a slash if the 2nd tab-separated field is a directory
 sd_aux () {
 	perl -na -F'\t' -e 'chomp; print; chomp($p = $F[1]); if (-d $p && substr($p, -1) ne "/") {print "/"} print $/'
 }
